67b7d3ce45c6df1dfa4fc37f2a465c2612b62c13
Michael Suo <darthsuo@gmail.com>
2012-04-07 14:56:34 -0400

    visualization signatures

--- classes.py
+++ classes.py
@@ -184,6 +184,18 @@ class State:
 		"""Returns list of (Permutation, ScoreMatrix) tuples, as passed to the initiator"""
 		pass
 		
+class Visualize:
+	"""
+	Takes structure and outputs it as visualize
+	"""
 
+	def viz_circle(self,structure):
+		"""Returns bmp of circular secondary structure graph"""
+		pass
+
+	def viz_planar(self,structure):
+		"""Return bmp of planar secondary structure graph"""
+		pass
 		
-		
+	def viz_arc(self,structure):
+		"""Return bmp of arc graph"""
