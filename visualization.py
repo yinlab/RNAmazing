16941b79affc1c85f8182429afef59baf954493a
Casey Grun <casey.grun@wyss.harvard.edu>
2012-04-07 16:05:18 -0400

    Moved visualization stuff to a separate file

---
+++ visualization.py
@@ -0,0 +1,15 @@
+class Visualize:
+	"""
+	Takes structure and outputs it as visualize
+	"""
+
+	def viz_circle(self,structure):
+		"""Returns bmp of circular secondary structure graph"""
+		pass
+
+	def viz_planar(self,structure):
+		"""Return bmp of planar secondary structure graph"""
+		pass
+		
+	def viz_arc(self,structure):
+		"""Return bmp of arc graph"""
