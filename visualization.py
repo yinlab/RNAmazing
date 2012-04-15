2066c789ec762fc7a2f32e8236411ec68ba59b1f
Michael Suo <michaelsuo@college.harvard.edu>
2012-04-15 17:17:11 -0400

    dot-bracket

--- visualization.py
+++ visualization.py
@@ -2,6 +2,19 @@ class Visualize:
 	"""
 	Takes structure and outputs it as visualize
 	"""
+	def viz_bracket(self, sstr, seq):
+		"""Returns string of secondary structure in dot-bracket notation"""
+		output = ""
+		#fill dots
+		for i in seq:
+			output += "."
+
+		#fill brackets
+		for i in sstr:
+			x,y = i
+			output = output[:x] + "(" + output[x+1:]
+			output = output[:y] + ")" + output[y+1:]
+		return output
 
 	def viz_circle(self,structure):
 		"""Returns bmp of circular secondary structure graph"""
