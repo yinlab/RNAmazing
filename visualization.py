809ce9f41d7a8140f346f7334471be237ac2c7c8
Angela Li <angelabyangela@gmail.com>
2012-04-25 23:35:37 -0400

    Cleaned chord diagram

--- visualization.py
+++ visualization.py
@@ -30,36 +30,35 @@ class Visualize:
 		canvasheight = 600
 		w = Canvas(master, width = canvaswidth, height = canvasheight)
 		w.pack()
+		w.configure(background = "white")
 
-		# Draw circle and tick marks
+		# Draw circle
 		w.create_oval(50, 50, 550, 550)
 		angle = (2 * math.pi) / len(seq)
-		r = 250
-		for i in range(len(seq)):
-			theta = i * angle
-			x = r * math.cos(theta) + (canvaswidth / 2)
-			y = r * math.sin(theta) + (canvasheight / 2)
-			w.create_oval(x - 2, y - 2, x + 2, y + 2, fill = "black")
 
-		# Draw bases
+		# Draw bases and tick marks
 		coords = []
 		for i, base in enumerate(seq):
 			theta = i * angle
+			# Draw bases
 			r = 275
 			x = r * math.cos(theta) + (canvaswidth / 2)
 			y = r * math.sin(theta) + (canvasheight / 2)
 			w.create_text(x, y, text = base)
+			# Create tick marks
 			r = 250
 			x2 = r * math.cos(theta) + (canvaswidth / 2)
 			y2 = r * math.sin(theta) + (canvasheight / 2)
+			w.create_oval(x2 - 2, y2 - 2, x2 + 2, y2 + 2, fill = "black", activewidth = 4)
+			# Save base location
 			coords.append({"x": x2, "y": y2})
 			
 		# Draw segments between bases
-		for base1, base2 in sstr:
-			w.create_line(coords[base1]["x"], coords[base1]["y"], coords[base2]["x"], coords[base2]["y"])
+		for b1, b2 in sstr:
+			w.create_line(coords[b1]["x"], coords[b1]["y"], coords[b2]["x"], coords[b2]["y"], activewidth = 3)
 
 		# Enter main event loop
-		mainloop()	
+		mainloop()
 
 	def viz_planar(self,structure):
 		"""Return bmp of planar secondary structure graph"""
